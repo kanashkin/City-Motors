58bf1fef6d687dd541813db5d0de99139dc7266c
Alexander <kanashkin_ad@mail.ru>
2023-12-05 19:25:35 +0300

    commit

--- js/app.js
+++ js/app.js
@@ -12,3 +12,19 @@ const autoBuySwiper = new Swiper('.auto-swiper', {
     slidesPerView: 4,
     loop: true,
 })
+
+const burgerMenu = () => {
+    const openBtn = document.querySelector('.burger')
+    const closeBtn = document.querySelector('.burger-close')
+    const burgerMenuBlock = document.querySelector('.burger-menu')
+
+    openBtn.onclick = function() {
+        burgerMenuBlock.classList.add('active')
+    }
+
+    closeBtn.onclick = function() {
+        burgerMenuBlock.classList.remove('active')
+    }
+}
+
+burgerMenu()
